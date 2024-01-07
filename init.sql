e45aef45f5f85a73900dc8a929ba27144c61f49e
Mrpablo98 <pablosandev@gmail.com>
2024-01-07 21:50:55 +0100

    Update Nike Air Zoom Pegasus 38 description

--- init.sql
+++ init.sql
@@ -26,6 +26,6 @@ INSERT INTO products (name, description, price) VALUES ('Product 1', 'Descriptio
 INSERT INTO products (name, description, price) VALUES ('Product 2', 'Description of Product 2', 19.99);
 INSERT INTO products (name, description, price) VALUES ('Laptop HP Pavilion', '15.6-inch, Intel Core i5, 8GB RAM, 256GB SSD', 799.99);
 INSERT INTO products (name, description, price) VALUES ('Samsung Galaxy S22', '6.5-inch display, 128GB storage, 5G capable', 999.00);
-INSERT INTO products (name, description, price) VALUES ('Nike Air Zoom Pegasus 38', 'Men\'s running shoes, breathable mesh, cushioned support', 129.99);
+INSERT INTO products (name, description, price) VALUES ('Nike Air Zoom Pegasus 38', 'Mens running shoes, breathable mesh, cushioned support', 129.99);
 INSERT INTO products (name, description, price) VALUES ('Apple iPad Pro', '11-inch, 256GB, Wi-Fi, Space Gray', 899.00);
 INSERT INTO products (name, description, price) VALUES ('Sony WH-1000XM4', 'Wireless noise-canceling headphones, 30-hour battery life', 349.99);
